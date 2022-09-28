4ee2b08afb6ac18c90c3375a29a68042bb2130ba
refszin <refwire@gmail.com>
2022-09-28 00:00:01 -0300

    Tables

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 import dotenv from "dotenv";
-
+// const connection = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
@@ -14,4 +16,6 @@ export default async function mongo () {
         console.error("Não foi possível se conectar", error)
         return error;    
     }
-}
+}const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
